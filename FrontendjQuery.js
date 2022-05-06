489ae3aa79e4cf4f5fc9844f6be8f11e2589cf9c
RuneHansen <rune9485@elev.tec.dk>
2022-05-06 11:32:49 +0200

    first commit

---
+++ FrontendjQuery.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    console.log('Siden er klar!')
+});
+
+$(document).ready(function(){
+    $('#input').on('click', function(){
+        console.log('Button clicked')
+    })
+});
