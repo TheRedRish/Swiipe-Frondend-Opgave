a7db2c75ed4ec34a7a83b098133d104e4ba30961
RuneHansen <rune9485@elev.tec.dk>
2022-05-06 14:13:12 +0200

    Errormessages showhide on submit. No dynamic updat

--- FrontendjQuery.js
+++ FrontendjQuery.js
@@ -13,3 +13,47 @@ $(function(){
     $('#submit-answer').html(firstname+lastname+status);
     });
 })
+
+
+$(function(){
+    $('#my-form').on('submit', function(){
+        var fname = $("#fname").val();
+        var lname = $("#lname").val();
+        var succes = false;
+
+        if (fname == '') {
+            $("#fname_error_message").html("Should contain only Characters");
+            $("#fname_error_message").show();
+        }
+        else {
+            $("#fname_error_message").hide();
+        }
+        if (lname == '') {
+            $("#lname_error_message").html("Should contain only Characters");
+            $("#lname_error_message").show();
+        }    
+        else {
+            $("#lname_error_message").hide();
+        }    
+        
+    })
+    
+})
+/*
+$("#fname").focusout(function(){
+    if (fname !== '') {
+       $("#fname_error_message").hide();
+    } else {
+       $("#fname_error_message").html("Should contain only Characters");
+       $("#fname_error_message").show();
+    }
+});
+
+$("#lname").focusout(function(){
+    if (lname !== '') {
+        $("#lname_error_message").hide();
+    } else {
+        $("#lname_error_message").html("Should contain only Characters");
+        $("#lname_error_message").show();
+    }
+});*/
